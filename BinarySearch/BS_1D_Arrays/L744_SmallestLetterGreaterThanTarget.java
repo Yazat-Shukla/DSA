40a8257857cc55e6743dcc351e1f9164e81d0c5c
Yazat-Shukla <vegmishra@gmail.com>
2026-07-13 10:21:31 +0530

    Need to cehck the logic behind the return

---
+++ BinarySearch/BS_1D_Arrays/L744_SmallestLetterGreaterThanTarget.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class L744_SmallestLetterGreaterThanTarget {
+    public static void main(String[] args) {
+        char[] letters = {'x','x','y','y'};
+        char target = 'x';
+        char ans = nextGreatestLetter(letters,target);
+        System.out.println(ans);
+    }
+
+    static char nextGreatestLetter(char[] letters, char target) {
+            int start = 0;
+            int end = letters.length-1;
+            if (target>= letters[letters.length-1]){
+                return letters[0];
+            }
+            while (start <=end){
+                int mid = start +(end-start)/2;
+                if (target<letters[mid]){
+                    end = mid -1;
+                } else {
+                    start = mid+1;
+                    // return letters[mid];
+                }
+            }
+            //figure out the logic behind this why
+            return letters[start % letters.length];
+        }
+
+    }
+
